d3cd9c54af730b68265f38966ffbb17349c89723
jsmoot93 <47538435+jsmoot93@users.noreply.github.com>
2019-03-12 16:06:22 -0400

    nav menu is done

--- nav-menu/menu.js
+++ nav-menu/menu.js
@@ -1,12 +1,8 @@
-const toggleMenu = () => {
-    // Toggle the "menu--open" class on your menu reference. 
-    menu.classList.toggle('menu--open');
+const toggleNav = () => {
+    nav.classList.toggle('nav--open');
   };
   
-  // Start Here: Create a reference to the ".menu" class
-  const menu = document.querySelector('.menu');
-  // create a reference to the ".menu-button" class
-  const menuButton = document.querySelector('.menu-button');
-  // Using your menuButton reference, add a click handler that calls toggleMenu
-  menuButton.addEventListener('click', toggleMenu);
+  const nav = document.querySelector('.nav');
+  const navButton = document.querySelector('.buttona');
+  navButton.addEventListener('click', toggleNav);
   
