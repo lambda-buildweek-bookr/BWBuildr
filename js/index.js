4b5c79e560c3469b5ca9bb4143244f36e473cd93
jsmoot93 <47538435+jsmoot93@users.noreply.github.com>
2019-03-12 19:33:46 -0400

    added myself to the about page

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const siteContent = {
+    "pics": {
+      "pic-1": "images/book-cluster.jpeg",
+      "pic-2": "images/book-library.jpeg",
+      "pic-3": "images/book-library2.jpeg",
+      "pic-edward": "images/edward.jpg",
+      "pic-ilya": "https://ca.slack-edge.com/T4JUEB3ME-UDHNR45HB-6b68d207fcb2-512",
+      "pic-davina": "https://ca.slack-edge.com/T4JUEB3ME-UERE6DSFN-5f49b5ee17af-512",
+      "pic-jamie": "https://ca.slack-edge.com/T4JUEB3ME-UDC6DCVQT-a1b55f16d1c2-512"
+  };
+  
+  const buy = document.getElementsByClassName("buy");
+  buy.setAttribute('src', siteContent["pics"]["pic-1"]);
--- nav-menu/menu.js
+++ nav-menu/menu.js
@@ -5,15 +5,3 @@ const toggleNav = () => {
   const nav = document.querySelector('.nav');
   const navButton = document.querySelector('.buttona');
   navButton.addEventListener('click', toggleNav);
-
-const siteContent = {
-  "pic": {
-    "pic-1": "images/book-cluster.jpeg",
-    "pic-2": "images/book-library.jpeg",
-    "pic-3": "images/book-library2.jpeg",
-    "pic-edward": "images/edward.jpg",
-    "pic-ilya": "https://ca.slack-edge.com/T4JUEB3ME-UDHNR45HB-6b68d207fcb2-512",
-    "pic-davina": "https://ca.slack-edge.com/T4JUEB3ME-UERE6DSFN-5f49b5ee17af-512"
-    "pic-jamie": "https://ca.slack-edge.com/T4JUEB3ME-UDC6DCVQT-a1b55f16d1c2-512"
-
-};
